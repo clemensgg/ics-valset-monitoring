911c621b71231587597412a6577b9f880836c2ad
clemensgg <csc@soundportal.at>
2023-10-23 07:25:52 +0200

    add config loader

---
+++ indexer/configLoader.js
@@ -0,0 +1,35 @@
+import { validateCONFIG } from './configValidate.js';
+
+let CONFIG;
+
+export async function loadConfig() {
+  if (CONFIG) {
+    return CONFIG;
+  }
+
+  const DEPLOYMENT = process.env.DEPLOYMENT ?? 'test';
+  let configModule;
+
+  try {
+    if (DEPLOYMENT === 'production') {
+      configModule = await import('./config.js');
+    } else {
+      configModule = await import('./config_test.js');
+    }
+    CONFIG = configModule.CONFIG;
+
+    // Validate Config
+    try {
+        const validatedConfig = validateCONFIG(CONFIG);
+        CONFIG = validatedConfig;
+    } catch (error) {
+        throw (error.message);
+    }
+
+    global.CONFIG = CONFIG;
+    console.log('>>> Loaded config for deployment mode: ' + DEPLOYMENT)
+  } catch (err) {
+    console.error(`Failed to load ${DEPLOYMENT} config:`, err);
+    throw err;
+  }
+}
