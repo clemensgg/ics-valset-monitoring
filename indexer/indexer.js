3932db7611664c9522d9c2a69c12804fe719e0f4
clemensgg <csc@soundportal.at>
2023-10-23 07:26:53 +0200

    migrate to postgres

--- indexer/indexer.js
+++ indexer/indexer.js
@@ -1,5 +1,3 @@
-
-// Import statements
 import { updateConsensusStateDB,
   initializeData,
   prepareChains,
@@ -7,30 +5,16 @@ import { updateConsensusStateDB,
   updateStakingValidatorsDB
 } from './db/update.js';
 import { getConsensusState } from './utils/utils.js';
+import { workers } from './setupWorkers.js'
 
-import { Worker } from 'worker_threads';
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-import { CONFIG } from './config.js'
-
-// Init Workers
-const currentFilePath = fileURLToPath(import.meta.url);
-const workerFilePath = path.resolve(path.dirname(currentFilePath), './db/updateStakingDatabaseWorker.js');
-
-const updateStakingDatabaseWorker = new Worker(workerFilePath);
-
-updateStakingDatabaseWorker.on('message', (message) => {
-  if (message === 'Done') {
-    console.log('Chain and validator data updated.');
-  } else if (message.startsWith('Error:')) {
-    console.error(`Worker Error: ${message}`);
-  }
-});
-
-updateStakingDatabaseWorker.on('error', (error) => {
-  console.error(`Worker Thread Error: ${error.message}`);
-});
+async function updateChainAndValidatorData() {
+  setInterval(() => {
+    console.log('Updating chain and validator data...');
+    workers.forEach((worker) => {
+      worker.postMessage('updateStakingValidatorsDB');
+    });
+  }, global.CONFIG.UPDATE_DB_FREQUENCY_MS);
+}
 
 async function startupRoutine() {
   console.log('Running startup routine...');
@@ -38,14 +22,6 @@ async function startupRoutine() {
   await updateStakingValidatorsDB(providerChainInfos, consumerChainInfos);
   console.log('Startup routine completed.');
   return true;
-}
-
-// Update Chain and Validator Data
-async function updateChainAndValidatorData() {
-  setInterval(async () => {
-    console.log('Updating chain and validator data...');
-    updateStakingDatabaseWorker.postMessage('updateStakingValidatorsDB');
-  }, CONFIG.UPDATE_DB_FREQUENCY_MS);
 }
 
 // Monitor Consensus State
@@ -57,6 +33,8 @@ async function consensusStateMonitor(chains, stakingValidators) {
 
 // Main Function
 async function main() {
+  // await loadConfig();
+
   console.log('Starting ics-valset-monitoring');
 
   // Initialize Data
@@ -84,7 +62,7 @@ async function pollConsensus(chains) {
     const consensusState = await getConsensusState(chain.rpcEndpoint, chain.chainId);
     
     if (consensusState) {
-      await updateConsensusStateDB(consensusState, CONFIG.RETAIN_STATES);
+      await updateConsensusStateDB(consensusState, global.CONFIG.RETAIN_STATES);
       console.log(`Updated consensusState for chain ${chain.chainId}`);
     } else {
       console.warn(`Error updating consensusState for chain ${chain.chainId}`);
@@ -98,3 +76,5 @@ main().catch((err) => {
   console.error('Uncaught error:', err);
   process.exit(1);
 });
+
+
